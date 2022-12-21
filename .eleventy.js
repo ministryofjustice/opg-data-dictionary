3a64a63754fb90622f8f68ba4872982f22770d57
Matt <matt.machell@digital.justice.gov.uk>
2022-12-21 15:02:18 +0000

    Remove unused RSS plugin (#20)

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const dateFilter = require('./src/filters/date-filter.js');
 const date24HourFilter = require('./src/filters/date24Hours-filter.js');
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' })
@@ -10,8 +9,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'src/CNAME': '' });
 
     eleventyConfig.addPassthroughCopy({ 'src/_data': 'data' });
-
-    eleventyConfig.addPlugin(pluginRss);
 
     eleventyConfig.addNunjucksGlobal("is_number", (str) => { return !isNaN(str) })
 
